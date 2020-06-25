d778e384df61fa0b54d2b51336c209acb647112c
Matheus <matias.vicio8@gmail.com>
2020-06-25 19:52:38 -0400

    Rest

---
+++ src/main/java/com/exercicioaula/exercicioaula/resources/CategoriaResource.java
@@ -0,0 +1,19 @@
+package com.exercicioaula.exercicioaula.resources;
+
+import jdk.nashorn.internal.ir.RuntimeNode;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String resposta(){
+        return "Rest funcionando";
+    }
+
+
+}
